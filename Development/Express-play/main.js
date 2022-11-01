74985fb5cd1c3c2eff11ebf894e47893233e83d2
Mai Vang <vmaineng@gmail.com>
2022-10-31 17:04:24 -0700

    play

---
+++ Development/Express-play/main.js
@@ -0,0 +1,8 @@
+// document.querySelector('button').addEventListener('click', apiRequest);
+
+// async function apiRequest() {
+//     const month = document.querySelector('input').value
+//     try {
+//         const response = await fetch("")
+//     }
+// }
--- Development/Express-play/server.js
+++ Development/Express-play/server.js
@@ -1,7 +1,41 @@
-const express = require('express');
+//depdendancies need to build server
+const express = require("express");
 const app = express();
-const cors = require('cors')
-const PORT = 8000
+const cors = require("cors");
+const PORT = 8000;
+
+app.use(cors());
+
+const protein = {
+  'meat': {
+    'name': "'chicken', 'beef', 'shrimp'",
+  },
+  'plant': {
+    'name': "'edamame', 'tofu'",
+  },
+  'unknown': {
+    'name': "'Not a protein source"
+  }
+};
 
 
-app.use(cors())
+
+//app is variable name we gave to express while the get method allows us to request something where its a file, route or data.
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:type', (req, res)=>{
+    const type = req.params.type.toLocaleLowerCase()
+    if(protein[type]){
+       res.json(protein[type])  
+    } else  {
+        res.json(holidays['unknown'])
+    }
+   
+})
+
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`GO TO PORT ${PORT}`)
+})
